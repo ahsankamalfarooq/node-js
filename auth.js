9005d952fca457a902a23f5c3bb595f46196739e
Wazir <ahsankamal25@gmail.com>
2023-04-08 00:16:44 +0500

    local module completed

---
+++ auth.js
@@ -0,0 +1,8 @@
+const register = function(username) {
+    console.log(`User ${username} has been registered`)
+}
+
+
+
+
+module.exports = register;
--- module.js
+++ module.js
@@ -1,18 +1,45 @@
 //module mean file the code which i am writing here will remain here not clash with any file , and if we want to use that so we will have to export it
 
 
-// npm module
+// npm module(which npm made by itself)
 
 // iifc
 
-const color = require('cli-color')
+// const color = require('cli-color')
+// console.log(color.red('hello'))
 
 // from where that require function came from so for that we hVE TO 
 // UNDERSTND THe immediately invoked function of JS (IIFE)
 
 
- ( function(name) {
-    console.log(name)
-}) ("Ali") //() for immediTE INVOKING bcz we didnt give any name
+ //( function(name) {
+  //  console.log(name)
+//}) ("Ali") //() for immediTE INVOKING bcz we didnt give any name
 
- 
+ //so what node js do is that is will wrap every file in to iife function that the const, var and let etc or any thing can never be used outside
+
+
+///////////////////////////////////////////////////////////
+// LOcal Module (which we made by ourselves)
+///////////////////////////////////////////////////////////
+
+//importing file from auth.js
+
+const reister = require('./auth')
+
+reister('ALi')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
