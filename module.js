564b5383b3a2ab1e4553f4b248c6f816f614603b
Wazir <ahsankamal25@gmail.com>
2023-04-08 00:04:02 +0500

    files created and sorted

---
+++ module.js
@@ -0,0 +1,18 @@
+//module mean file the code which i am writing here will remain here not clash with any file , and if we want to use that so we will have to export it
+
+
+// npm module
+
+// iifc
+
+const color = require('cli-color')
+
+// from where that require function came from so for that we hVE TO 
+// UNDERSTND THe immediately invoked function of JS (IIFE)
+
+
+ ( function(name) {
+    console.log(name)
+}) ("Ali") //() for immediTE INVOKING bcz we didnt give any name
+
+ 
