7fdca6feb7a915630c5bb6950db52c163b7af099
Wazir <ahsankamal25@gmail.com>
2023-04-20 23:57:36 +0500

    server created with node.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello")
+////////////////////////////////////////////////////////
+//HTTTP MODULE
+
+const http = require('http')
+
+const app = http.createServer((req, res) => {
+
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log("Listening on port 3000")
+})
+
+////////////////////////////////////////////////////////
