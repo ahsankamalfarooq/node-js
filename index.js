4b428f446f958c57e1094e1c91e164b199b52d63
Wazir <ahsankamal25@gmail.com>
2023-04-21 00:15:40 +0500

    pushed public files with server with all functionality

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
 //HTTTP MODULE
 
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 const app = http.createServer((req, res) => {
-
+    fs.readFile(path.join(__dirname, 'public', "index.html"), (err, content) => {
+        if(err) {
+            throw err
+        }
+        //
+        res.end(content)
+    })
 })
 
 const PORT = process.env.PORT || 3000
