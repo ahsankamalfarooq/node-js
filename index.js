35f98c2b85bf758f738833ea98472a73f5478cfb
Wazir <ahsankamal25@gmail.com>
2023-05-15 00:42:59 +0500

    completed with nodejs and its small website

--- index.js
+++ index.js
@@ -6,13 +6,80 @@ const fs = require('fs')
 const path = require('path')
 
 const app = http.createServer((req, res) => {
-    fs.readFile(path.join(__dirname, 'public', "index.html"), (err, content) => {
-        if(err) {
-            throw err
-        }
-        //
-        res.end(content)
+
+    res.writeHead(200, {
+        'content-Type': 'text/html'
+        // 'content-Type': 'text/plain'
     })
+
+    // if(req.url === '/') {
+    //     fs.readFile(path.join(__dirname, 'public', "index.html"), (err, content) => {
+    //         if(err) {
+    //             throw err
+    //         }
+    //         //
+    //         res.end(content)
+    //     })
+    // } else if(req.url === '/about') {
+    //     fs.readFile(path.join(__dirname, 'public', "about.html"), (err, content) => {
+    //         if(err) {
+    //             throw err
+    //         }
+    //         //
+    //         res.end(content)
+    //     })
+    // }
+
+
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+
+    let contentType = 'text/html'
+
+    let ext = path.extname(filePath)
+    if(!ext) {
+        filePath += '.html'
+    }
+
+    switch (ext) {
+        case '.css':
+            contentType = 'text/css'
+            
+            break
+        case '.js':
+            contentType = 'text/javascript'
+    
+            break
+        default:
+            contentType = 'text/html'
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if(err) {
+        //     res.writeHead(500)
+        //     res.end("Error!!!")
+        // }
+        ////OR/////
+        fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data)=> {
+            if(err) {
+                res.writeHead(500)
+                res.end("Error!!!")
+            } else {
+                res.writeHead(404 ,{
+                    'Content-Type': contentType
+                })
+                res.end(data)
+            }
+                
+        })
+
+    } else{
+        res.writeHead(200, {
+            'Content-Type' : contentType
+        })
+        res.end(content)
+    }
+    })
+
 })
 
 const PORT = process.env.PORT || 3000
